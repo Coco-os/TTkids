16168ec70023d4902a01fb03990c6f93902e1e32
AyozeRuano <ayozeruanoalcantara@gmail.com>
2024-12-15 17:53:24 +0000

    Add files via upload

--- Channel.java
+++ Channel.java
@@ -1,44 +1,45 @@
+import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Channel {
-    private List<Subscription> memberships;
+    private List<Membership> memberships;
     private List<User> subscribers;
-    private List<User> owners;
+    private User owner;
     private List<Video> videos;
 
     private String channelName;
     private String description;
     private final LocalDate startDate;
-    private String category; // tambien se puede hacer un enumeration
+    private CategoryType category; // tambien se puede hacer un enumeration
 
-    public Channel(String channelName, String description, LocalDate startDate, String category) {
+    public Channel(String channelName, String description, LocalDate startDate, CategoryType category, Adult parent) {
         this.channelName = channelName;
         this.description = description;
         this.startDate = startDate;
         this.category = category;
-        this.memberships = new ArrayList<Suscription>();
+        this.memberships = new ArrayList<Membership>();
         this.subscribers = new ArrayList<User>();
-        this.owners = new ArrayList<User>();
+        this.owner = parent;
         this.videos = new ArrayList<Video>();
     }
 
 
 
-    public List<Subscription> getSubscriptions() {
+    public List<Membership> getMemberships() {
         return this.memberships;
     }
 
-    public Subscription getSubscription(int id) {
+    public Membership getMembership(int id) {
         return this.memberships.get(id); // SE ACCEDERIA CON ID ?
     }
 
-    public void addSubscription(Subscription subscription) {
+    public void addMembership(Membership subscription) {
         this.memberships.add(subscription);
     }
 
-    public void deleteSubscription(int id) {
+    public void deleteMembership(int id) {
         this.memberships.remove(id); // CON ID ?
     }
 
@@ -54,81 +55,68 @@ public class Channel {
         return this.videos.get(id); // SE ACCEDERIA CON ID ?
     }
 
-    public void addVideo( // atributos de Video ) {
-        Video video = //;
+    public void addVideo(String title, String description, File file) {
+        Video video = new Video(title, description, file);
         this.videos.add(video);
+}
+
+public void deleteVideo(int id) {
+    this.videos.remove(id); // CON ID ?
+}
+
+
+
+public List<User> getSubscriptions() {
+    return this.subscribers;
+}
+
+public User getSubscriber(int id) {
+    return this.subscribers.get(id); // SE ACCEDERIA CON ID ?
+}
+
+public void addSubscriber(User subscriber) {
+    this.subscribers.add(subscriber);
+}
+
+public void deleteSubscriber(int id) {
+    this.subscribers.remove(id); // CON ID ?
+}
+
+
+    public User getOwner() {
+        return owner;
     }
 
-    public void deleteVideo(int id) {
-        this.videos.remove(id); // CON ID ?
+    public void setOwner(User owner) {
+        this.owner = owner;
     }
-
-
-
-    public List<User> getSubscriptions() {
-        return this.subscribers;
-    }
-
-    public Subscription getSubscription(int id) {
-        return this.subscribers.get(id); // SE ACCEDERIA CON ID ?
-    }
-
-    public void addSubscription(Subscription subscription) {
-        this.subscribers.add(subscription);
-    }
-
-    public void deleteSubscription(int id) {
-        this.subscribers.remove(id); // CON ID ?
-    }
-
-
-
-    public List<User> getSubscriptions() {
-        return this.owners;
-    }
-
-    public Subscription getSubscription(int id) {
-        return this.owners.get(id); // SE ACCEDERIA CON ID ?
-    }
-
-    public void addSubscription(Subscription subscription) {
-        this.owners.add(subscription);
-    }
-
-    public void deleteSubscription(int id) {
-        this.owners.remove(id); // CON ID ?
-    }
-
-
-
-
 
     public String getChannelName() {
-        return channelName;
-    }
+    return channelName;
+}
 
-    public void setChannelName(String channelName) {
-        this.channelName = channelName;
-    }
+public void setChannelName(String channelName) {
+    this.channelName = channelName;
+}
 
-    public String getDescription() {
-        return description;
-    }
+public String getDescription() {
+    return description;
+}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+public void setDescription(String description) {
+    this.description = description;
+}
 
-    public LocalDate getStartDate() {
-        return startDate;
-    }
+public LocalDate getStartDate() {
+    return startDate;
+}
 
-    public String getCategory() {
-        return category;
-    }
+public CategoryType getCategory() {
+    return category;
+}
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+public void setCategory(CategoryType category) {
+    this.category = category;
+}
 }
 
